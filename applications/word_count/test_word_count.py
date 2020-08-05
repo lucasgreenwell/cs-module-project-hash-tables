0cdca40b7a2634d464f77476a082fce76fda832d
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-08-04 23:11:17 -0600

    need to finish word_count still but it's bedtime

--- applications/word_count/test_word_count.py
+++ applications/word_count/test_word_count.py
@@ -12,7 +12,8 @@ class TestWordCount(unittest.TestCase):
         x = word_count("Hello    hello")
         self.assertTrue(x == {"hello": 2})
 
-        x = word_count('Hello, my cat.  And my cat doesn\'t say "hello" back.')
+        x = word_count('Hello, my cat.  And my cat doesnt say hello back.')
+        print(x)
         self.assertTrue(x == {'hello': 2, 'my': 2, 'cat': 2, 'and': 1, "doesn't": 1, 'say': 1, 'back': 1})
 
         x = word_count('This is a test of the  Emergency  Broadcast  Network. This is only a test.')
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,24 @@
 def word_count(s):
     # Your code here
+    if len(s) < 1:
+        return {}
+    words = s.split()
+    characters_remove = ['"', ':', ';', ',', '.', '-', '+', '=', '/',  '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    for removal in characters_remove:
+        for word in words:
+            if removal in word:
+                word.replace(())
+                word = "".join(chars)
+
+
+    words = [word.lower() for word in words]
+    word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    return word_counts
 
 
 
